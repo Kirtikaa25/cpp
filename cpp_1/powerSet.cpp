c36fbdd6011a09d9ce68eda7cb5eb51a9920e2b2
Kirtika Gautam <100124517+Kirtikaa25@users.noreply.github.com>
2023-12-08 01:50:04 +0530

    Update  powerSet.cpp

---
+++ cpp_1/powerSet.cpp
@@ -0,0 +1,22 @@
+void print(string s,vector<string>& sub){
+    int n=s.length();
+    int m=1<<n;
+    for(int num=0;num<m;num++){
+        string s2;
+        for(int i=0;i<n;i++){
+            if(num &(1<<i)){
+              s2+=s[i];
+            }
+            sub.push_back(s2);
+        }
+    }
+}
+int main(){
+    string s;
+    cin>>s;
+    vector<string> sub;
+    print(s,sub);
+    for(auto it:sub){
+        cout<<it<<endl;
+    }
+}
