e8d1c48989d3d4be55780971b0bb0a9d1d9d972f
Kirtika Gautam <100124517+Kirtikaa25@users.noreply.github.com>
2023-12-08 22:25:08 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,7 +1,7 @@
  #include <bits/stdc++.h>
-    using namespace std;
-    #define ll long long
-    #define mod 1000000007
+ using namespace std;
+ #define ll long long
+ #define mod 1000000007
 void solve()
 {
    
