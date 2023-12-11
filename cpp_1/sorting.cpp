175633775634af2404b5b09a5270c54f5b5a09e2
Kirtika Gautam <100124517+Kirtikaa25@users.noreply.github.com>
2023-12-12 01:56:59 +0530

    Update sorting.cpp

--- cpp_1/sorting.cpp
+++ cpp_1/sorting.cpp
@@ -1,1 +1,32 @@
+#include <bits/stdc++.h
+int func(int low,int high,vector<int>& arr){
+    int i=low;
+    int j=high;
+    while(i<j){
+        while(i<high && arr[low]>=arr[i]){
+          i++;
+        }
+        while(j>low && arr[low]<arr[j]){
+          j--;
+        }
+        if(i<j) swap(arr[i],arr[j]);
+    }
+    swap(arr[low],arr[j]);
+    return j;
+}
+void qs(int low,int high,vector<int>& arr){
+    if(low<high){
+        int pI=func(low,high,arr);
+        qs(low,pI-1,arr);
+        qs(pI+1,high,arr);
+    }
+}
+vector<int> quickSort(vector<int> arr)
+
+{
+    int low=0;
+    int high=arr.size()-1;
+    qs(low,high,arr);
+    return arr;
+}
 
