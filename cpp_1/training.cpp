434d9026bf22598e967baf6e604a29fecaf0e751
Kirtika Gautam <100124517+Kirtikaa25@users.noreply.github.com>
2023-12-15 20:11:07 +0530

    Update training.cpp

--- cpp_1/training.cpp
+++ cpp_1/training.cpp
@@ -15,3 +15,37 @@ int training(int n, vector<vector<int>> &points) {
    }
     return dp[n-1][3];
 }
+
+
+// recursive
+int solve(int day,int last,vector<vector<int>> &points,vector<vector<int>>& dp){
+  if(day==0){
+    int maxi=0;
+    for(int i=0;i<3;i++){
+      if(i!=last){
+        maxi=max(maxi,points[0][i]);
+      }
+    }
+    return dp[0][last]=maxi;
+  }
+  if(dp[day][last]!=-1) return dp[day][last];
+  int maxi=0;
+  for(int i=0;i<3;i++){
+    if(i!=last){
+      int point=solve(day-1,i,points,dp)+points[day][i];
+      maxi=max(maxi,point);
+    }
+  }
+  return dp[day][last]=maxi;
+}
+int ninjaTraining(int n, vector<vector<int>> &points) {
+  vector<vector<int>> dp(n,vector<int>(4,-1));
+  for(auto it:dp){
+    for(auto it1:it){
+      cout<<it1<<" ";
+    }
+    cout<<endl;
+  }
+  return solve(n-1,3,points,dp);
+
+}
