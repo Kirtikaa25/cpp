3245f2634f1930c5e58710b49d4044510d5aad38
Kirtika Gautam <100124517+Kirtikaa25@users.noreply.github.com>
2023-12-21 00:14:46 +0530

    Update knapsack.cpp

--- cpp_1/knapsack.cpp
+++ cpp_1/knapsack.cpp
@@ -3,9 +3,9 @@ int cutRod(vector<int> &price, int n) {
   vector<int> prev(n+1,0);
   for(int i=0;i<=n;i++) prev[i]=i*price[0];
   for(int i=1;i<n;i++){
-	  for(int j=1;j<=n;j++){
-		  int ntake=prev[j];
-		  int take=0;
+    for(int j=1;j<=n;j++){
+	int ntake=prev[j];
+      int take=0;
 		  if(i+1<=j) take=price[i]+prev[j-(i+1)];
 		  prev[j]=max(ntake,take);
 	  }
